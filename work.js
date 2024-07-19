76c42cbd7be0181427b90620cb3237c4f8335773
Joshua Kim <joshk.5793@gmail.com>
2024-07-19 18:16:47 -0400

    520 detect capital

--- work.js
+++ work.js
@@ -1,36 +1,36 @@
 // 1507. Reformat Date
 
-var reformatDate = function(date) {
-    const monthsMapping = {
-        Jan: '01',
-        Feb: '02',
-        Mar: '03',
-        Apr: '04',
-        May: '05',
-        Jun: '06',
-        Jul: '07',
-        Aug: '08',
-        Sep: '09',
-        Oct: '10',
-        Nov: '11',
-        Dec: '12'
-    };
-    const [date, month, year] = str.split(' ');
-    const updatedDate = (function () {
-        let store = '';
-        for (let index = 0; index < date.length; index++) {
-            if (date[index] > 'a') {
-                break;
-            }
-            store += date[index];
-        }
-        if (store.length === 1) {
-            store = `0${store}`;
-        }
-        return store;
-    })();
-    return `${year}-${monthsMapping[month]}-${updatedDate}`;
-};
+// var reformatDate = function(date) {
+//     const monthsMapping = {
+//         Jan: '01',
+//         Feb: '02',
+//         Mar: '03',
+//         Apr: '04',
+//         May: '05',
+//         Jun: '06',
+//         Jul: '07',
+//         Aug: '08',
+//         Sep: '09',
+//         Oct: '10',
+//         Nov: '11',
+//         Dec: '12'
+//     };
+//     const [date, month, year] = str.split(' ');
+//     const updatedDate = (function () {
+//         let store = '';
+//         for (let index = 0; index < date.length; index++) {
+//             if (date[index] > 'a') {
+//                 break;
+//             }
+//             store += date[index];
+//         }
+//         if (store.length === 1) {
+//             store = `0${store}`;
+//         }
+//         return store;
+//     })();
+//     return `${year}-${monthsMapping[month]}-${updatedDate}`;
+// };
 
 date = "20th Oct 2052"
 // console.log(reformatDate(date))
@@ -47,4 +47,27 @@ var findWords = function(words) {
 words = ["Hello","Alaska","Dad","Peace"]
 // words = ["omk"]
 // words = ["adsdf","sfd"]
-console.log(findWords(words))
+// console.log(findWords(words))
+
+
+// 520. Detect Capital
+
+var detectCapitalUse = function(word) {
+    const capitalAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    let count = 0
+    for (let i = 0; i < word.length; i++) {
+        if (capitalAlpha.includes(word[i])) {
+            count++
+        }
+    }
+    if (count === word.length) {
+        return true
+    } else {
+        return false
+    }
+};
+
+// word = "USA"
+word = "FlaG"
+console.log(detectCapitalUse(word))
