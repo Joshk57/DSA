537025a2be8a123a12bf1e9fd8b1d3465cca6337
Joshua Kim <joshk.5793@gmail.com>
2024-07-27 15:39:45 -0400

    solved 523

--- work.js
+++ work.js
@@ -118,7 +118,19 @@ strs = ["aba","cdc","eae"]
 //523. Continuous Subarray Sum
 
 var checkSubarraySum = function(nums, k) {
-    
+    let hash  = new Map();
+    let i = 0;
+    let sum = 0;
+    hash.set(0, 0)
+    while(i < nums.length) {
+        sum += nums[i] % k;
+        if(!hash.has(sum % k)) hash.set(sum % k, i+1);
+        else {
+            if (i - hash.get(sum % k)  + 1>= 2) return true;
+        }
+        i++;
+    }
+    return false;    
 };
 
 nums = [23,2,4,6,7], k = 6
