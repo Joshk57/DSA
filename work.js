9158a054b50eaa7050d1b13fbad321f178a7f14e
Joshua Kim <joshk.5793@gmail.com>
2024-08-10 15:50:28 -0400

    solved generate paranthesis

--- work.js
+++ work.js
@@ -226,7 +226,26 @@ console.log(mergeTwoLists(list1, list2))
 // 22. Generate paranthesis
 
 var generateParenthesis = function(n) {
-    
+    const res = [];
+
+    function dfs(openP, closeP, s) {
+        if (openP === closeP && openP + closeP === n * 2) {
+            res.push(s);
+            return;
+        }
+
+        if (openP < n) {
+            dfs(openP + 1, closeP, s + "(");
+        }
+
+        if (closeP < openP) {
+            dfs(openP, closeP + 1, s + ")");
+        }
+    }
+
+    dfs(0, 0, "");
+
+    return res;     
 };
 
 n = 3
