0638f9b1d2113b1dfc0a75eebe316f8de1db9cd7
Joshua Kim <joshk.5793@gmail.com>
2024-08-24 15:48:48 -0400

    solved removing duplicates from sorted aray

--- work.js
+++ work.js
@@ -364,7 +364,16 @@ head = [1,2,3,4,5], k = 2
 // 26. Remove Duplicates from Sorted Array
 
 var removeDuplicates = function(nums) {
-    
+    let i = 1;
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i - 1]) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+
+    return i;      
 };
 
 nums = [1,1,2]
